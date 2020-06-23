814bba1bcc63d16806c10385eed7ae1fc1170320
Lareta <lareta064@gmail.com>
2020-06-23 08:21:25 +0300

    header1

--- src/js/main.js
+++ src/js/main.js
@@ -169,5 +169,55 @@ $(document).ready(function () {
 
         })
     }
+    const selectBlock = document.querySelectorAll('.select-block')
+
+    for (let i = 0; i < selectBlock.length; i++) {
+
+        selectBlock[i].addEventListener('click', function (e) {
+
+            const outInput = selectBlock[i].querySelector('.filters-form-input')
+            const itemOptions = selectBlock[i].querySelectorAll('.select-option')
+            let optionsDrop = selectBlock[i].querySelector('.select-block-drop')
+            let arrowOpenDrop = selectBlock[i].querySelector('.input-arrow')
+
+
+            if (e.target == outInput || e.target == arrowOpenDrop) {
+                e.stopPropagation()
+
+                for (let j = 0; j < selectBlock.length; j++) {
+                    if (j != i) {
+                        selectBlock[j].querySelector('.select-block-drop').classList.add('hide')
+                        selectBlock[j].querySelector('.input-arrow').classList.remove('rotate')
+                    }
+                }
+
+                if (optionsDrop.classList.contains('hide')) {
+
+                    arrowOpenDrop.classList.add('rotate')
+                    optionsDrop.classList.remove('hide')
+
+                } else {
+
+                    optionsDrop.classList.add('hide')
+                    arrowOpenDrop.classList.remove('rotate')
+                }
+
+            }
+
+            const itemOptionsArray = Array.from(itemOptions)
+
+            for (let i = 0; i < itemOptionsArray.length; i++) {
+
+                if (e.target == itemOptionsArray[i]) {
+                    e.stopPropagation()
+
+                    const selectValue = itemOptionsArray[i].textContent
+                    outInput.value = selectValue
+                    optionsDrop.classList.add('hide')
+                    arrowOpenDrop.classList.remove('rotate')
+                }
+            }
+        })
+    }
 
 })
